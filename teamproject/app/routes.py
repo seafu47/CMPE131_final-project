31fdb79eeb53c11f281151733f81e5102e076a50
Seafu47 <jiaheng.fang@sjsu.edu>
2022-04-26 01:31:35 -0700

    Added upload picture tester

--- teamproject/app/routes.py
+++ teamproject/app/routes.py
@@ -1,10 +1,17 @@
 from flask import render_template, flash, url_for, redirect, request
 from flask_login import login_user, login_required, current_user, logout_user
+import os
+from werkzeug.utils import secure_filename
 
 from app import app, bcrypt, db
 from app.forms import RegisterForm, LoginForm
 from app.models import User
 
+
+AllOWED_EXTENSIONS = (['png','jpg'])
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in AllOWED_EXTENSIONS
 
 @app.route('/', methods=['GET', 'POST'])
 @login_required
@@ -14,6 +21,10 @@ def index():
         'section2',
         'section3'
     }
+    if request.method == "POST":
+        if request.form.get('action1') == 'Upload':
+            pass
+
 
     return render_template('index.html',
                            title='Home',
@@ -68,3 +79,34 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('login'))
+
+
+@app.route('/upload')
+def upload_form():
+    return render_template('upload.html')
+
+@app.route('/upload', methods = ['POST'])
+@login_required
+def upload_image():
+    if 'file' not in request.files:
+        flash("No file found")
+        return redirect(request.url)
+    file = request.files['file']
+
+    if file.filename == '':
+        flash("No imagine was selected")
+        return redirect(request.url)
+
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save = (os.path.join(app.config['UPLOAD_FOLDER'],filename))
+        flash("Image successfully uploaded")
+        return render_template('upload.html', filename=filename)
+    else:
+        flash("Allowed image types are .png file")
+        return redirect(request.url)
+
+
+@app.route('/display/<filename>')
+def display_image(filename):
+    return redirect(url_for('static', filename='uploads/' + filename))
